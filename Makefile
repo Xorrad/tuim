87fa72fcd9c496e6060d94124115b5e9b722fcbc
Xorrad <monsieurs.aymeric@gmail.com>
2023-12-11 07:39:28 +0100

    test: add unit tests for tuim::string

--- Makefile
+++ Makefile
@@ -4,7 +4,13 @@ LDFLAGS   := -L/usr/lib -lstdc++ -lm -ldl -ltinfo
 SRC 	  := $(wildcard examples/*.cpp)
 TARGETS   := $(SRC:examples/%.cpp=%)
 
-all: $(TARGETS) clean
+all: $(TARGETS) test clean
+
+test: tests/test.cpp
+	@clear
+	@mkdir -p bin
+	$(CXX) $(CXXFLAGS) -o bin/$@ $< $(LDFLAGS)
+	./bin/$@
 
 %: examples/%.cpp
 	@clear
---
+++ tests/test.cpp
@@ -0,0 +1,137 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+#include "../tuim.hpp"
+
+TEST_SUITE("string") {
+
+    TEST_CASE("length") {
+        CHECK(tuim::string::length("abcde") == 5);
+        CHECK(tuim::string::length("12345€") == 6);
+        CHECK(tuim::string::length("12ßEP😀A") == 7);
+    }
+
+    TEST_CASE("fill") {
+        CHECK(tuim::string::fill("khâzad-dûm", 0) == "");
+        CHECK(tuim::string::fill("22-cycle", 1) == "22-cycle");
+        CHECK(tuim::string::fill("hello world", 3) == "hello worldhello worldhello world");
+        CHECK(tuim::string::fill("a", 5) == "aaaaa");
+    }
+    
+    TEST_CASE("is_style") {
+        CHECK(tuim::string::is_style("&vehicle", 0));
+        CHECK(tuim::string::is_style("hello #_ff00ffworld", 6));
+        CHECK_FALSE(tuim::string::is_style("&computer", 1));
+        CHECK_FALSE(tuim::string::is_style("l/&ion", 2));
+    }
+    
+    TEST_CASE("extract_style") {
+        // TODO: need to init tuim before testing (because it checks for style tags)
+    }
+
+    TEST_CASE("strip_styles") {
+        // TODO: need to init tuim before testing (because it checks for style tags)
+    }
+
+    TEST_CASE("utf8_to_int") {
+        CHECK(tuim::string::utf8_to_int("a") == 97);
+        CHECK(tuim::string::utf8_to_int("£") == 49827);
+        CHECK(tuim::string::utf8_to_int("æ") == 50086);
+        CHECK(tuim::string::utf8_to_int("😀") == 4036991104);
+        CHECK(tuim::string::utf8_to_int(tuim::string::int_to_utf8(4036991104)) == 4036991104);
+    }
+    
+    TEST_CASE("int_to_utf8") {
+        CHECK(tuim::string::int_to_utf8(97) == "a");
+        CHECK(tuim::string::int_to_utf8(49827) == "£");
+        CHECK(tuim::string::int_to_utf8(50086) == "æ");
+        CHECK(tuim::string::int_to_utf8(4036991104) == "😀");    
+        CHECK(tuim::string::int_to_utf8(tuim::string::utf8_to_int("😀")) == "😀");
+    }
+
+    TEST_CASE("is_printable") {
+        CHECK(tuim::string::is_printable(tuim::string::utf8_to_int("a")));
+        CHECK(tuim::string::is_printable(tuim::string::utf8_to_int("ß")));
+        CHECK(tuim::string::is_printable(tuim::string::utf8_to_int("€")));
+        CHECK(tuim::string::is_printable(tuim::string::utf8_to_int("😀")));
+        for(int i = 0; i < 0x20; i++)
+            CHECK_FALSE(tuim::string::is_printable(i));
+        // CHECK_FALSE(tuim::string::is_printable(127));
+        // CHECK_FALSE(tuim::string::is_printable(49792));
+    }
+
+    TEST_CASE("char_length") {
+        CHECK(tuim::string::char_length('a') == 1);
+        CHECK(tuim::string::char_length("é"[0]) == 2);
+        CHECK(tuim::string::char_length("€"[0]) == 3);
+        CHECK(tuim::string::char_length("😀"[0]) == 4);
+    }
+
+    TEST_CASE("to_lowercase") {
+        CHECK(tuim::string::to_lowercase(tuim::string::utf8_to_int("A")) == tuim::string::utf8_to_int("a"));
+        CHECK(tuim::string::to_lowercase(tuim::string::utf8_to_int("É")) == tuim::string::utf8_to_int("é"));
+        CHECK(tuim::string::to_lowercase(tuim::string::utf8_to_int("Æ")) == tuim::string::utf8_to_int("æ"));
+        CHECK(tuim::string::to_lowercase(tuim::string::utf8_to_int("Ñ")) == tuim::string::utf8_to_int("ñ"));
+        
+        CHECK(tuim::string::to_lowercase(tuim::string::utf8_to_int("a")) == tuim::string::utf8_to_int("a"));
+        CHECK(tuim::string::to_lowercase(tuim::string::utf8_to_int("€")) == tuim::string::utf8_to_int("€"));
+        CHECK(tuim::string::to_lowercase(tuim::string::utf8_to_int("😀")) == tuim::string::utf8_to_int("😀"));
+    }
+
+    TEST_CASE("is_alphanumeric") {
+        CHECK_FALSE(tuim::string::is_alphanumeric('-'));
+        CHECK_FALSE(tuim::string::is_alphanumeric('/'));
+        CHECK_FALSE(tuim::string::is_alphanumeric('('));
+        CHECK_FALSE(tuim::string::is_alphanumeric(']'));
+
+        for(int c = 'a'; c <= 'z'; c++)
+            CHECK(tuim::string::is_alphanumeric(c));
+        for(int c = 'A'; c <= 'Z'; c++)
+            CHECK(tuim::string::is_alphanumeric(c));
+        for(int c = '0'; c <= '9'; c++)
+            CHECK(tuim::string::is_alphanumeric(c));
+        for(int c = 50048; c <= 50070; c++) // À-Ö
+            CHECK(tuim::string::is_alphanumeric(c));
+        for(int c = 50072; c <= 50102; c++) // Ø-ö
+            CHECK(tuim::string::is_alphanumeric(c));
+        for(int c = 50104; c <= 50111; c++) // ø-ÿ
+            CHECK(tuim::string::is_alphanumeric(c));
+    }
+
+    TEST_CASE("is_vowel") {
+        CHECK(tuim::string::is_vowel('a'));
+        CHECK(tuim::string::is_vowel('e'));
+        CHECK(tuim::string::is_vowel('i'));
+        CHECK(tuim::string::is_vowel('o'));
+
+        CHECK_FALSE(tuim::string::is_vowel('z'));
+        CHECK_FALSE(tuim::string::is_vowel('j'));
+        CHECK_FALSE(tuim::string::is_vowel('['));
+        CHECK_FALSE(tuim::string::is_vowel('*'));
+        // TODO: test other alphabets
+    }
+}
+
+TEST_SUITE("items") {
+
+    TEST_CASE("calc_text_width") {
+        CHECK(tuim::calc_text_width("hello world") == 11);
+        CHECK(tuim::calc_text_width("præss ënt€r to stärt!\n") == 22);
+        CHECK(tuim::calc_text_width("#ffff00player name:&r xorrad") == 19);
+        CHECK(tuim::calc_text_width("#_ffff00player name:&r xorrad") == 19);
+        CHECK(tuim::calc_text_width("#ff00ff;cursor") == 6);
+        CHECK(tuim::calc_text_width("#_ff00ff;cursor") == 6);
+        CHECK(tuim::calc_text_width("&acursor") == 8);
+        CHECK(tuim::calc_text_width("&_acursor") == 9);
+    }
+
+}
+
+int main(int argc, char* argv[]) {
+    tuim::init(argc, argv);
+    printf("\033[?1049l"); // disabled alternate buffer
+    tuim::native::show_user_inputs();
+    tuim::native::set_cursor_visible(true);
+    int res = doctest::Context(argc, argv).run();
+    tuim::delete_context();
+    return res;
+}
