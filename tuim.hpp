338e05d5fef3538068b68a28176d9068c3d2f5c2
Xorrad <monsieurs.aymeric@gmail.com>
2025-05-12 02:04:48 +0200

    feat(tuim): add basic native terminal functions

--- tuim.hpp
+++ tuim.hpp
@@ -7,8 +7,8 @@
 *                                                                           *
 ****************************************************************************/
 
-#ifndef TUIM_CPP
-#define TUIM_CPP
+#ifndef TUIM_HPP
+#define TUIM_HPP
 
 namespace tuim {
     class Context;
@@ -22,6 +22,16 @@ namespace tuim {
 #include <iostream>
 #include <string>
 #include <string_view>
+
+#ifdef __linux__
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#elif _WIN32
+#error "Windows is not supported yet."
+#else
+#error "This operating system is not supported."
+#endif
 
 /***********************************************************
 *                    MACROS/DEFINES                        *
@@ -110,16 +120,31 @@ namespace tuim {
 
     void SetTitle(std::string_view title); // Change the terminal title
     void SetCursorVisibility(bool visible); // Change the cursor visibility
+    void SetFullscreen(bool fullscreen); // Change the terminal to full screen
 
     void Update(Key key); // Update the frame depending on the key pressed
     void Clear(); // Clear the current frame buffer
     void Display(); // Draw the current frame buffer to the terminal
 
     /***********************************************************
-    *                    TERMINAL FUNCTIONS                    *
+    *                TERMINAL/NATIVE FUNCTIONS                 *
     ***********************************************************/
 
-    //
+    namespace Terminal {
+        bool IsUserInputsVisible(); // Determine if user inputs are visible
+        vec2 GetTerminalSize(); // Determine the size in cells of the terminal window.
+
+        void SetTitle(std::string_view title); // Change the terminal title
+        void SetCursorVisibility(bool visible); // Change the terminal visibility
+        void SetUserInputsVisibility(bool visible); // Change the user inputs visibility
+        void SetAlternateBuffer(bool enabled); // Toggle the terminal alternate buffer
+        void SetCursorPos(const vec2& pos); // Change the cursor position
+
+        void Clear(); // Clear the whole terminal
+        void ClearLine(); // Clear the terminal current line
+        void ClearEnd(); // Clear the terminal from the cursor to the end
+        void ClearStyles(); // Remove any active ANSI style
+    }
 
     /***********************************************************
     *                   CONTAINERS FUNCTIONS                   *
@@ -175,9 +200,102 @@ namespace tuim {
 ***********************************************************/
 
 /***********************************************************
-*                    WINDOW FUNCTIONS                      *
+*                    CONTEXT FUNCTIONS                     *
 ***********************************************************/
 
+void tuim::CreateContext() {
 
+}
 
-#endif
+void tuim::CreateContext(int argc, char* argv[]) {
+    tuim::CreateContext();
+}
+
+void tuim::DeleteContext() {
+
+}
+
+tuim::Context* tuim::GetCtx() {
+    
+}
+
+void tuim::SetTitle(std::string_view title) {
+
+}
+
+void tuim::SetCursorVisibility(bool visible) {
+
+}
+
+void tuim::SetFullscreen(bool fullscreen) {
+
+}
+
+void tuim::Update(tuim::Key key) {
+
+}
+
+void tuim::Clear() {
+
+}
+
+void tuim::Display() {
+
+}
+
+/***********************************************************
+*                 TERMINAL/NATIVE FUNCTIONS                *
+***********************************************************/
+
+bool tuim::Terminal::IsUserInputsVisible() {
+    // TODO: keep track of the cursor visibility in the context.
+    return false;
+}
+
+tuim::vec2 tuim::Terminal::GetTerminalSize() {
+    winsize size;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+    return vec2(size.ws_col, size.ws_row);
+}
+
+void tuim::Terminal::SetTitle(std::string_view title) {
+    printf("\033]0;%s\007", title.data());
+}
+
+void tuim::Terminal::SetCursorVisibility(bool visible) {
+    printf("\033[?25%c", (visible ? 'h' : 'l'));
+}
+
+void tuim::Terminal::SetUserInputsVisibility(bool visible) {
+    termios term;
+    tcgetattr(0, &term);
+    if (visible) term.c_lflag |= ECHO;
+    else term.c_lflag &= ~ECHO;
+    tcsetattr(0, TCSANOW, &term);
+}
+
+void tuim::Terminal::SetAlternateBuffer(bool enabled) {
+    printf("\033[?1049%c", (enabled ? 'h' : 'l'));
+}
+
+void tuim::Terminal::SetCursorPos(const tuim::vec2& pos) {
+    printf("\033[%d;%dH", pos.y+1, pos.x+1);
+}
+
+void tuim::Terminal::Clear() {
+    printf("\033[2J\033[0m\033[H");
+}
+
+void tuim::Terminal::ClearLine() {
+    printf("\033[2K");
+}
+
+void tuim::Terminal::ClearEnd() {
+    printf("\033[0J");
+}
+
+void tuim::Terminal::ClearStyles() {
+    printf("\033[0m");
+}
+
+#endif // TUIM_HPP
