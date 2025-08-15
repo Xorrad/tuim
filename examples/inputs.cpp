a79b1e3a2a079d39b4d63f40725209c1afe368cc
Xorrad <monsieurs.aymeric@gmail.com>
2025-08-15 22:04:46 +0200

    feat(demo): add demo for input components

--- examples/inputs.cpp
+++ examples/inputs.cpp
@@ -4,55 +4,55 @@
 SignalHandler signalHandler;
 
 int main(int argc, char* argv[]) {    
+    
     // Setup tuim settings.
     tuim::CreateContext(argc, argv);
     tuim::SetTitle("tuim - inputs");
     tuim::SetFramerate(1.f);
 
-    tuim::DefineStyle('b', tuim::Style::BOLD);        // &b for bold
-    tuim::DefineStyle('U', tuim::Style::UNDERLINE);  // &U for underline
-    tuim::DefineStyle('i', tuim::Style::ITALIC);     // &i for italic
+    // Define global variables for each input.
+    std::string str1 = "abcdefg1234$*ù€_-{{}}[]";
+    std::string str2 = "12345";
+    std::string str3 = "abcdefg1234";
+    int number1 = 10;
+    float number2 = 5.f;
 
-    std::string str = "";
-    std::string input1 = "xorrade";
-    int i = 10;
-    float f = 5.f;
-
+    // Start the main loop.
     char32_t keyCode = 0;
     while (keyCode != tuim::Key::F1) {
         keyCode = tuim::PollKeyCode();
         tuim::Update(keyCode);
         tuim::Clear();
-        
-        tuim::BeginContainer("#container1", "", tuim::vec2(100, 100));
 
-        if (keyCode != 0 && !tuim::IsKeyPressed(tuim::Key::LEFT) && !tuim::IsKeyPressed(tuim::Key::RIGHT) && !tuim::IsKeyPressed(tuim::Key::UP) && !tuim::IsKeyPressed(tuim::Key::DOWN) && !tuim::IsKeyPressed(tuim::Key::ENTER) && !tuim::IsKeyPressed(tuim::Key::ESCAPE) && !tuim::IsKeyPressed(tuim::Key::BACKSPACE))
-            str += tuim::Utf8Char32ToString(keyCode);
-        tuim::Print("{}\n\n", str);
-
-        tuim::Print("&bThis has a blue background.&r\n");
-        tuim::Print("&&This has a blue background.&r\n");
-        tuim::Print("#_0000ffThis has a blue background.&r\n");
-        tuim::Print("##This has a blue background.&r\n");
-
-        if (tuim::TextInput("#input1", "Username: {}", &input1, tuim::INPUT_TEXT_FLAGS_CONFIRM_ON_ENTER | tuim::INPUT_TEXT_FLAGS_NUMERIC_ONLY)) {
-            tuim::Print(" Changed");
-        }
-        tuim::Print("\n");
-        for (int i = 0; i < input1.size(); i++)
-            tuim::Print("{:#04x}\t{:#04x}\n", input1[i], str[i]);
-        
-        if (tuim::IntSlider("#input2", "Int: {} {}", &i, 0, 50, 1, 50)) {
+        // Display a text input that accepts any characters and return true when enter is pressed.
+        if (tuim::TextInput("#input1", "Text: {}", &str1, tuim::INPUT_TEXT_FLAGS_CONFIRM_ON_ENTER)) {
             tuim::Print(" Changed");
         }
         tuim::Print("\n");
         
-        if (tuim::FloatSlider("#input3", "Float: {} {:.2f}", &f, 0.f, 5.f, 0.01f, 50)) {
+        // Display a text input that only accepts digits and return true when enter is pressed.
+        if (tuim::TextInput("#input2", "Only digits: {}", &str2, tuim::INPUT_TEXT_FLAGS_CONFIRM_ON_ENTER | tuim::INPUT_TEXT_FLAGS_NUMERIC_ONLY)) {
             tuim::Print(" Changed");
         }
         tuim::Print("\n");
-
-        tuim::EndContainer();
+        
+        // Display a text input that only accepts digits and letters and return true when enter is pressed.
+        if (tuim::TextInput("#input3", "Only digits and letters: {}", &str3, tuim::INPUT_TEXT_FLAGS_CONFIRM_ON_ENTER | tuim::INPUT_TEXT_FLAGS_ALPHANUMERIC_ONLY)) {
+            tuim::Print(" Changed");
+        }
+        tuim::Print("\n");
+        
+        // Display a integer slider with min, max, step and width (of the progress bar) parameters.
+        if (tuim::IntSlider("#input4", "Int: {} {}", &number1, 0, 50, 1, 50)) {
+            tuim::Print(" Changed");
+        }
+        tuim::Print("\n");
+        
+        // Display a decimal slider with min, max, step and width (of the progress bar) parameters.
+        if (tuim::FloatSlider("#input3", "Float: {} {:.2f}", &number2, 0.f, 5.f, 0.01f, 50)) {
+            tuim::Print(" Changed");
+        }
+        tuim::Print("\n");
 
         tuim::Display();
     }
